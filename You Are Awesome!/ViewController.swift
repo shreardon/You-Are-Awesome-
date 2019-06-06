1d6ba207597fe446dc4d713b3756dd55af846ab1
Shannon Reardon <shannonreardon@shannons-macbook-pro.local>
2019-06-06 19:21:49 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Shannon Reardon on 6/6/19.
+//  Copyright © 2019 Shannon Reardon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
